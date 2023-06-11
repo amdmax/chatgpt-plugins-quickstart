ea4086a841656a3b8d0727f63d0b6de0ab9829a9
Maksym Diabin <maksym_diabin@epam.com>
2023-06-11 10:21:16 +0300

    updated for a softdev company

--- main.py
+++ main.py
@@ -7,27 +7,32 @@ from quart import request
 app = quart_cors.cors(quart.Quart(__name__), allow_origin="https://chat.openai.com")
 
 # Keep track of todo's. Does not persist if Python session is restarted.
-_TODOS = {}
+_developers = {}
 
-@app.post("/todos/<string:username>")
-async def add_todo(username):
+@app.post("/developers/<string:username>")
+async def add_dev(username):
     request = await quart.request.get_json(force=True)
-    if username not in _TODOS:
-        _TODOS[username] = []
-    _TODOS[username].append(request["todo"])
+    name = request["name"]
+    if name not in _developers:
+        _developers[name] = []
+    
+    print(name)
+    print(username)
+
+    _developers[name].append(name)
     return quart.Response(response='OK', status=200)
 
-@app.get("/todos/<string:username>")
-async def get_todos(username):
-    return quart.Response(response=json.dumps(_TODOS.get(username, [])), status=200)
+@app.get("/developers/<string:username>")
+async def get_developers(username):
+    return quart.Response(response=json.dumps(_developers.get(username, [])), status=200)
 
-@app.delete("/todos/<string:username>")
-async def delete_todo(username):
+@app.delete("/developers/<string:username>")
+async def delete_dev(username):
     request = await quart.request.get_json(force=True)
-    todo_idx = request["todo_idx"]
+    dev_idx = request["dev_id"]
     # fail silently, it's a simple plugin
-    if 0 <= todo_idx < len(_TODOS[username]):
-        _TODOS[username].pop(todo_idx)
+    if 0 <= dev_idx < len(_developers[username]):
+        _developers[username].pop(dev_idx)
     return quart.Response(response='OK', status=200)
 
 @app.get("/logo.png")
